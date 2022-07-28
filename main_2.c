3d646d122f6f34a119fe07d294e93a8eb08b1e48
Hardbass1991 <midailsorridente@gmail.com>
2022-07-27 21:45:31 -0700

    fixed string length issue. Now it works as intented

--- main_2.c
+++ main_2.c
@@ -5,22 +5,28 @@
 int main(int argc, char *argv[], char *envp[])
 {
 	char *var_name;
-	int n;
 
 	if (argc != 2)
 	{
 		printf("Usage: _getenv [environment variable]\n");
 		return 1;
 	}
-	n = strlen(argv[1]);
 	for (char **env = envp; *env != 0; env++)
 	{
 		char *thisEnv = *env;
-		var_name = malloc(n + 1);
-		strncpy(var_name, thisEnv, n);
+		int i = 0;
+
+		while (thisEnv[i])
+		{
+			i++;
+			if (thisEnv[i] == '=')
+				break;
+		}
+		var_name = malloc(i + 1);
+		strncpy(var_name, thisEnv, i);
 		if (!strcmp(var_name, argv[1]))
 		{
-			printf("%s\n", thisEnv + n + 1);
+			printf("%s\n", thisEnv + i + 1);
 			free(var_name);
 			break;
 		}
