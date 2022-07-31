98fd66bada5dd0843e1b97e26ea64803bc1a0c58
Hardbass1991 <midailsorridente@gmail.com>
2022-07-31 16:10:52 -0700

    added functionality. Now it checks for stored commands

--- path.c
+++ path.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "main.h"
-int main()
+int main(int argc, char *argv[])
 {
-	char *line, *this_name, *token;
-	size_t i = 0;
+	char *line, *this_name, *token, *check_com;
+	size_t i = 0, n, command_found = 0;
 	extern char **environ;
 	list_t *paths, *ptr;
+	struct stat st;
+	
+	if (argc != 2)
+	{
+		printf("Usage: ./path [bash command]\n");
+		return -1;
+	}
 
 	while (environ[i])
 	{
-		printf("%sAAAAA\n", environ[i]);
 		this_name = malloc(5);
 		strncpy(this_name, environ[i], 4);
-		printf("%s\n", this_name);
 		if (!strcmp(this_name, "PATH"))
 		{
 			line = malloc(strlen(environ[i]) + 1 - 5);
@@ -23,7 +32,7 @@ int main()
 		}
 		i++;
 	}
-	printf("%s\n", line);
+	//printf("%s\n", line);
 	paths = NULL;
 	token = strtok(line, ":");
 	while (token != NULL)
@@ -41,5 +50,22 @@ int main()
 		ptr = ptr->next;
 	}
 	free(line);
-	return 0;
+	ptr = paths;
+	while (ptr)
+	{
+		n = strlen(ptr->str);
+		check_com = malloc(n + strlen(argv[1]) + 2);
+		strncpy(check_com, ptr->str, n);
+		check_com[n] = '/';
+		strcpy(check_com + n + 1, argv[1]);
+		printf("%s\n", check_com);
+		if (stat(check_com, &st) == 0)
+		{
+			command_found = 1;
+			break;
+		}
+		ptr = ptr->next;
+	}
+	printf("%ld\n", command_found);
+	return (command_found);
 }
